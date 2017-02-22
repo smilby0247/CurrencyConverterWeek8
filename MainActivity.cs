709c7f9daefc860a3790a4b1381bf081aeb6b653
Shaun Milby <smilby0247@my.sctd.edu>
2017-02-21 21:33:44 -0500

    Week 8 stuff
    Few changes made.

---
+++ MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using CurrencyConverter.Models;
+
+namespace CurrencyConverter
+{
+    [Activity(Label = "CurrencyConverter", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        
+
+        public List<Converter> otherValues { get; set; }
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            // list of countries in relationships to the USD
+
+            otherValues = new List<Converter>();
+            otherValues.Add(new Converter {Country = "CAD", valueInUSD = 1.31 });
+            otherValues.Add(new Converter { Country = "GBP", valueInUSD = 0.92 });
+            otherValues.Add(new Converter { Country = "EUR", valueInUSD = 0.54 });
+
+            
+        }
+
+
+    }
+
+   
+}
+
